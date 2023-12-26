fafdb5513c0e1d77b6d84db70355fd823162e3cd
rodwin espiritu <rodwin.espiritu@maroonstudios.com>
2023-12-26 15:38:46 +0800

    feat: allow users to upload PDF

--- app/api/utils/vectorStoreUtils.js
+++ app/api/utils/vectorStoreUtils.js
@@ -1,19 +1,29 @@
 import { FaissStore } from "langchain/vectorstores/faiss";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { TextLoader } from "langchain/document_loaders/fs/text";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 export async function splitTextDocument (file) {
     try {
-        const loader = new TextLoader(file);
-        const text = await loader.load();
-    
-        const splitter = new RecursiveCharacterTextSplitter({
-            chunkSize: 500,
-            chunkOverlap: 50
-        })
-    
-        const output = await splitter.splitDocuments(text)
+        let output = ''
+
+        if (file.type === 'text/plain') {
+            const loader = new TextLoader(file)
+            const text = await loader.load()
+            const splitter = new RecursiveCharacterTextSplitter({
+                chunkSize: 500,
+                chunkOverlap: 50
+            })
+        
+            output = await splitter.splitDocuments(text)
+        } else if (file.type === 'application/pdf') {
+            const loader = new PDFLoader(file);
+            output = await loader.load();
+        } else {
+            console.log('Error')
+            return
+        }
     
         return output
     } catch (error) {
